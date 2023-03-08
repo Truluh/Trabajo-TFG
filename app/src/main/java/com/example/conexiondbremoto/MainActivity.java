0ee5103f95dea820972316c636f84f52d7ab05d4
Manue3 <eltrabajofake@gmail.com>
2023-03-08 15:21:02 +0100

    Registro

--- app/src/main/java/com/example/conexiondbremoto/MainActivity.java
+++ app/src/main/java/com/example/conexiondbremoto/MainActivity.java
@@ -2,89 +2,43 @@ package com.example.conexiondbremoto;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.Message;
 import android.os.StrictMode;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import java.io.File;
 import java.sql.*;
 
 public class MainActivity extends AppCompatActivity {
-    private EditText etUsuario, etContrasena;
-    private Button btnLogin;
-    private Button btnRegistro;
-    private static String driver = "com.mysql.jdbc.Driver";
-    private static String url = "jdbc:mysql://db4free.net/tienda_mascotas?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC&useSSL=false";
-    private static String user = "tienda_admin";
-    private static String password = "adminTienda2023";
-    public static Connection cnx = null;
+private EditText et;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        et = findViewById(R.id.et);
 
-        // realizamos la conexion con la base de datos
-        realizarConexion();
+        java.sql.Connection cnx = null;
+        String driver = "com.mysql.jdbc.Driver";
+        String url = "jdbc:mysql://db4free.net/tienda_mascotas?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC&useSSL=false";
+        String user = "tienda_admin";
+        String password = "adminTienda2023";
 
-        btnRegistro = findViewById(R.id.btnRegistrarseLogin);
-        etUsuario = findViewById(R.id.etUsuarioLogin);
-        etContrasena = findViewById(R.id.etContrasenaLogin);
-        btnLogin = findViewById(R.id.btnLogin);
-
-        //click del boton login
-        btnLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Statement sentencia = null;
-                try {
-                    sentencia = cnx.createStatement();
-                    ResultSet resul = sentencia.executeQuery("select * from usuarios where usuario = '"+etUsuario.getText()+"'");
-                    while (resul.next()) {
-                        String usuario = resul.getString("usuario");
-                        String contrasena = resul.getString("contrasena");
-
-                        if(contrasena.equals(etContrasena.getText().toString())){
-                            Toast.makeText(MainActivity.this, "Usuario y contraseña coinciden", Toast.LENGTH_SHORT).show();
-                            Intent i = new Intent(getApplicationContext(), PaginaPrincipal.class);
-                            startActivity(i);
-                        }else{
-                            Toast.makeText(MainActivity.this, "datos incorrectos", Toast.LENGTH_SHORT).show();
-
-                        }
-                    }
-                } catch (SQLException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-        });
-
-        btnRegistro.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent a = new Intent(MainActivity.this,Registro.class);
-                startActivity(a);
-            }
-        });
-
-
-    }
-
-    private void realizarConexion(){
         try {
             Class.forName(driver);
             StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
             StrictMode.setThreadPolicy(policy);
             cnx = DriverManager.getConnection(url, user, password);
-            Log.e("CONEXION A BD", " conexion establecida");
+
+            Statement sentencia = cnx.createStatement();
+            ResultSet resul = sentencia.executeQuery("select * from table_name");
+
+            while (resul.next()) {
+                et.setText(et.getText() + " "+resul.getString("column1"));
+            }
         } catch (Exception e) {
-            Log.e("CONEXION A BD", " error al conectarse a la base de datos");
+            et.setText(""+e);
+
         }
     }
 
